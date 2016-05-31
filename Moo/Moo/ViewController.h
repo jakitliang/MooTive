b77cf976ffc3c73f7724933570f1f33f3c60a9a8
Jakit <jakit@xiaoqiandemac-mini.local>
2016-05-31 15:11:28 +0800

    Create Project

---
+++ Moo/Moo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Moo
+//
+//  Created by Jakit on 16/5/31.
+//  Copyright © 2016年 Jakit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Moo/MooTests/MooTests.m
@@ -0,0 +1,39 @@
+//
+//  MooTests.m
+//  MooTests
+//
+//  Created by Jakit on 16/5/31.
+//  Copyright © 2016年 Jakit. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MooTests : XCTestCase
+
+@end
+
+@implementation MooTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
